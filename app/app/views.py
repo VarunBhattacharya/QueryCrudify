930bbdfed5cbb593a6b474d4e07657a2e9730a03
VarunBhattacharya <personnel.varunb@gmail.com>
2023-11-13 19:57:58 +0530

    Update files for database migration

--- app/app/views.py
+++ app/app/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from crudOp.models import BasicTable
 
 def mainPage(request):
-    return render(request, "index.html")
+    basicTab = BasicTable.objects.all()
+    dataTransfer = {
+        'basicTable': basicTab
+    }
+    return render(request, "index.html", dataTransfer)
