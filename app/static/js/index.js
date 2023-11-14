8e15193eccab259c79438620c10522a7cd8592fb
VarunBhattacharya <personnel.varunb@gmail.com>
2023-11-14 20:47:12 +0530

    Update styling and resources

---
+++ app/static/js/index.js
@@ -0,0 +1,12 @@
+function addButtonFunction() 
+{
+    var x = document.getElementById("addParamsForm");
+    if (x.style.display === "none") 
+    {
+      x.style.display = "block";
+    } 
+    else 
+    {
+      x.style.display = "none";
+    }
+  }
